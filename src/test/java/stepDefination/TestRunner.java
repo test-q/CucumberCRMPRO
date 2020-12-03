e8bd299736b309f30cc9e879703566eb4d375ecc
test-q <sr.test85@gmail.com>
2020-12-03 20:02:32 +0530

    update pom.xml

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(features="src/test/resources/Features/login.feature", glue= {"stepDefination"},
 monochrome = true, // Console output in readable format
 plugin = {"pretty", "html:test-output", "json:Json-Report/report.json", "junit:Junit-Report/report.xml"},
-dryRun = false, //Check any undefined/pending step is available or not
+dryRun = true, //Check any undefined/pending step is available or not
 strict = true
 
 //plugin = {"pretty", "html:test-output"},
